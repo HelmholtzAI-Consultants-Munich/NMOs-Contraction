dafb18b59b55a4b9d876946cf79ead3dc5b20e2c
donatella-cea <donatella.cea@helmholtz-muenchen.de>
2024-07-05 17:35:12 +0200

    add autocorrelation analysis, fix plot bug, add combobox

--- utils/feature_extraction.py
+++ utils/feature_extraction.py
@@ -1,15 +1,22 @@
 import pandas as pd
 import numpy as np
+import statsmodels.api as sm
 import tsfresh as tsf
 
+from ACF import (check_stationarity, 
+                differienciate_time_series,
+                compute_significant_positive_lags,
+                plot_acf_and_significant_lags,
+                compute_fundamental_interval_with_decay_threshold
+                )
 
-def create_feature_in_dataframe(data_array, contraction_class, threshold=0.5):
+
+def create_feature_in_dataframe(data_array, df_fundamental_interval, threshold=0.5):
     """This function creates a dataframe with all the features for each file.
-     data_array = the array with the data that have already been pre-processed - in main  data_no_processed,
-     data_weak_processed, and data_strong_processed
-     file_name_no = list of filenames per category
-     contraction_class = 0, 1, 2 respectively for no, weak and strong contraction
-     threshold = float to fix threshold to compute counts above threshold
+    Arguments:
+        - data_array (Array) = the array with the data that have already been pre-processed
+        - df_fundamentl_period (DataFrame): 
+        - threshold (float): thresholdvalue to compute counts above threshold
      """
     scaling_time = 0.097
     df_data = pd.DataFrame()
@@ -50,7 +57,6 @@ def create_feature_in_dataframe(data_array, contraction_class, threshold=0.5):
             # Large standard deviation
 
         df_0 = pd.DataFrame([[data_array[file][0],
-                              contraction_class,
                               np.median(energy_single_time_series),
                               np.median(mean_single_time_series),
                               np.median(count_above_threshold_single_time_series),
@@ -60,7 +66,6 @@ def create_feature_in_dataframe(data_array, contraction_class, threshold=0.5):
                               np.median(count_above_mean_single_time_series)
                               ]],
                             columns=['Video name',
-                                     'Class',
                                      'Energy',
                                      'Mean',
                                      '%_of_count_above_threshold',
@@ -71,5 +76,60 @@ def create_feature_in_dataframe(data_array, contraction_class, threshold=0.5):
                                      ])
 
         df_data = pd.concat([df_data, df_0], axis=0, ignore_index=True)
+    # Merge ACF dataframe with other features
+    if df_fundamental_interval is not None:
+        df_data = pd.merge(df_data, df_fundamental_interval, on='Video name', how='outer')
 
     return df_data
+
+
+def extract_acf_values(
+        data_array, 
+        image_path,
+        ):
+    """This function computes the ACF values and extract the fundamental interval for each observation
+    Arguments:
+    - data (array): the array with the data that have already been pre-processed (especially detrending is important)
+    Returns:
+    - fundamenta_interval (series): column containing the fundamental interval for each observation
+    """
+    fundamental_interval_single_time_series = []
+    for file in range(len(data_array)):
+        data = data_array[file][1] 
+        # Step 1: Preprocessing and avarage over the 10 time series (representing the 10 different points of the organoids)
+        avaraged_data = [sum(x) / len(data) for x in zip(*data)]
+
+        # Step 2: Check stationarity and differienciate
+        stationarity_flag = check_stationarity(avaraged_data)
+        avaraged_data = differienciate_time_series(stationarity_flag, avaraged_data)
+
+        # Step 3: Calculate ACF values and confidence intervals using Bartlett's formula
+        # If you want constant confidence bands, set bartlett_confint=False
+        acf_values, confint = sm.tsa.acf(avaraged_data,
+                                        alpha=0.05, #confidence interval 95%
+                                        nlags=6000,
+                                        bartlett_confint=True,
+                                        )
+        
+        # Step 4 and 5: Recenter CI, necessary to have the confidence intervals centered to zero (the confint are centered around the ACF value)
+        # Find the significant lags where ACF values are outside the confidence intervals 
+        significant_lags, centered_ci = compute_significant_positive_lags(acf_values=acf_values,
+                                                                        confint=confint,
+                                                                        )
+        plot_acf_and_significant_lags(
+            input_data=avaraged_data,
+            input_name= data_array[file][0],
+            image_path=image_path,
+            acf_values=acf_values,
+            significant_lags=significant_lags,
+            plotted_lags=6000,
+            alpha=0.05,
+            )   
+
+        #Step 6 and 7:
+        fundamental_interval = compute_fundamental_interval_with_decay_threshold(acf_values, centered_ci, significant_lags)
+        fundamental_interval_single_time_series.append((data_array[file][0], fundamental_interval))
+    # create DataFrame using 
+    df_fundamental_interval_single_time_series = pd.DataFrame(fundamental_interval_single_time_series, columns = ['Video name', 'Fundamental interval [s]'])
+    return df_fundamental_interval_single_time_series
+ 
